f3bbbfcec6efbbd873a56da133ed280d701831fa
Jeremy Coutellec <j.coutellec@pyres.com>
2023-12-19 15:39:18 +0100

    feat(NFCWrite): view write scrollable + Input raw

--- lib/src/nfc_feature/nfc_write_view.dart
+++ lib/src/nfc_feature/nfc_write_view.dart
@@ -21,6 +21,10 @@ class _NfcWriteViewState extends State<NfcWriteView> {
   String? _writeResult;
   bool _pooling = false;
   late List<TextEditingController> _textController;
+  late TextEditingController _identifierController;
+  late TextEditingController _payloadController;
+  late TextEditingController _typeController;
+  late int _dropButtonValue;
 
   @override
   void initState() {
@@ -28,6 +32,15 @@ class _NfcWriteViewState extends State<NfcWriteView> {
     _textController = [
       TextEditingController.fromValue(const TextEditingValue(text: ''))
     ];
+
+    _identifierController =
+        TextEditingController.fromValue(const TextEditingValue(text: ""));
+    _payloadController =
+        TextEditingController.fromValue(const TextEditingValue(text: ""));
+    _typeController =
+        TextEditingController.fromValue(const TextEditingValue(text: ""));
+    _dropButtonValue =
+        ndef.TypeNameFormat.values.indexOf(ndef.TypeNameFormat.empty);
   }
 
   void writeNDEF() async {
@@ -111,81 +124,189 @@ class _NfcWriteViewState extends State<NfcWriteView> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      const SizedBox(height: 40),
-      Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 40),
-        child: Row(
-          children: [
-            Expanded(
-              child: Column(
-                children: [
-                  for (int i = 0; i < _textController.length; i++)
-                    Row(
-                      children: [
-                        Expanded(
+    return LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints viewportConstraints) {
+      return SingleChildScrollView(
+          child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: viewportConstraints.maxHeight,
+              ),
+              child: IntrinsicHeight(
+                  child: Column(children: [
+                Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 40),
+                    child: Row(children: [
+                      Expanded(
+                        flex: 1,
+                        child: Column(
+                          children: [
+                            for (int i = 0; i < _textController.length; i++)
+                              Row(
+                                children: [
+                                  Expanded(
+                                    child: TextFormField(
+                                      decoration: const InputDecoration(
+                                          labelText: 'text'),
+                                      controller: _textController[i],
+                                    ),
+                                  ),
+                                  if (i == _textController.length - 1)
+                                    ElevatedButton(
+                                      child: const Text('+'),
+                                      onPressed: () {
+                                        setState(() {
+                                          _textController
+                                              .add(TextEditingController());
+                                        });
+                                      },
+                                    )
+                                  else
+                                    ElevatedButton(
+                                      child: const Text('-'),
+                                      onPressed: () {
+                                        setState(() {
+                                          _textController.removeAt(i);
+                                        });
+                                      },
+                                    ),
+                                ],
+                              ),
+                          ],
+                        ),
+                      ),
+                    ])),
+                const Divider(),
+                Row(children: [
+                  Expanded(
+                      flex: 1,
+                      child: ElevatedButton(
+                        onPressed: !_pooling
+                            ? () async {
+                                writeNDEF();
+                              }
+                            : null,
+                        child: Text(_pooling
+                            ? 'Please scan your tag ...'
+                            : 'Write NDEF'),
+                      )),
+                ]),
+                const Divider(),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 40),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: DropdownButton(
+                          value: _dropButtonValue,
+                          items: const [
+                            DropdownMenuItem(
+                              value: 0,
+                              child: Text('empty'),
+                            ),
+                            DropdownMenuItem(
+                              value: 1,
+                              child: Text('nfcWellKnown'),
+                            ),
+                            DropdownMenuItem(
+                              value: 2,
+                              child: Text('media'),
+                            ),
+                            DropdownMenuItem(
+                              value: 3,
+                              child: Text('absoluteURI'),
+                            ),
+                            DropdownMenuItem(
+                                value: 4, child: Text('nfcExternal')),
+                            DropdownMenuItem(
+                                value: 5, child: Text('unchanged')),
+                            DropdownMenuItem(
+                              value: 6,
+                              child: Text('unknown'),
+                            ),
+                          ],
+                          onChanged: (value) {
+                            setState(() {
+                              _dropButtonValue = value as int;
+                            });
+                          },
+                        ),
+                      ),
+                      Expanded(
+                          flex: 1,
                           child: TextFormField(
                             decoration:
-                                const InputDecoration(labelText: 'text'),
-                            controller: _textController[i],
-                          ),
+                                const InputDecoration(labelText: 'identifier'),
+                            validator: (v) {
+                              return v!.trim().length % 2 == 0
+                                  ? null
+                                  : 'length must be even';
+                            },
+                            controller: _identifierController,
+                          )),
+                      Expanded(
+                        flex: 1,
+                        child: TextFormField(
+                          decoration: const InputDecoration(labelText: 'type'),
+                          validator: (v) {
+                            return v!.trim().length % 2 == 0
+                                ? null
+                                : 'length must be even';
+                          },
+                          controller: _typeController,
                         ),
-                        if (i == _textController.length - 1)
-                          ElevatedButton(
-                            child: const Text('+'),
-                            onPressed: () {
-                              setState(() {
-                                _textController.add(TextEditingController());
-                              });
+                      ),
+                      Expanded(
+                          flex: 1,
+                          child: TextFormField(
+                            decoration:
+                                const InputDecoration(labelText: 'payload'),
+                            validator: (v) {
+                              return v!.trim().length % 2 == 0
+                                  ? null
+                                  : 'length must be even';
                             },
-                          )
-                        else
-                          ElevatedButton(
-                            child: const Text('-'),
-                            onPressed: () {
-                              setState(() {
-                                _textController.removeAt(i);
-                              });
-                            },
-                          ),
-                      ],
-                    ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-      const SizedBox(height: 10),
-      const Divider(),
-      const SizedBox(height: 10),
-      Row(children: [
-        Expanded(
-            flex: 1,
-            child: ElevatedButton(
-              onPressed: !_pooling
-                  ? () async {
-                      writeNDEF();
-                    }
-                  : null,
-              child: Text(_pooling ? 'Please scan your tag ...' : 'Write NDEF'),
-            )),
-        Expanded(
-            flex: 1,
-            child: ElevatedButton(
-              onPressed: !_pooling
-                  ? () async {
-                      writeMailBox();
-                    }
-                  : null,
-              child:
-                  Text(_pooling ? 'Please scan your tag ...' : 'Write MailBox'),
-            ))
-      ]),
-      Expanded(
-        flex: 1,
-        child: Text(_writeResult != null ? 'Result: $_writeResult' : ''),
-      )
-    ]);
+                            controller: _payloadController,
+                          ))
+                    ],
+                  ),
+                ),
+                // Center(
+                //   child: ElevatedButton(
+                //     child: const Text('OK'),
+                //     onPressed: () {
+                //       if ((_formKey.currentState as FormState).validate()) {
+                //         Navigator.pop(
+                //             context,
+                //             ndef.NDEFRecord(
+                //                 tnf: ndef.TypeNameFormat.values[_dropButtonValue],
+                //                 type: (_typeController.text).toBytes(),
+                //                 id: (_identifierController.text).toBytes(),
+                //                 payload: (_payloadController.text).toBytes()));
+                //       }
+                //     },
+                //   ),
+                // ),
+                Row(children: [
+                  Expanded(
+                      flex: 1,
+                      child: ElevatedButton(
+                        onPressed: !_pooling
+                            ? () async {
+                                writeMailBox();
+                              }
+                            : null,
+                        child: Text(_pooling
+                            ? 'Please scan your tag ...'
+                            : 'Write MailBox'),
+                      ))
+                ]),
+                Expanded(
+                  flex: 1,
+                  child:
+                      Text(_writeResult != null ? 'Result: $_writeResult' : ''),
+                )
+              ]))));
+    });
   }
 }
